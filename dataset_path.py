138399c73506f85f3710b5f0df694b6737471219
Bagge <470752945@qq.com>
2018-01-11 10:13:15 +0800

    update

---
+++ dataset_path.py
@@ -0,0 +1,19 @@
+# root根目录， patterns匹配文件格式， single_level是否进行目录深层次查找
+import fnmatch, os
+def allFiles(root, patterns = '*', single_level = False, yield_folders = False):
+    patterns = patterns.split(';')
+    for path, subdirs, files in os.walk(root):
+        if yield_folders:
+           #add subdirs to the tail of files
+           files.extend(subdirs)
+        files.sort()
+        for name in files:
+            for pattern in patterns:
+                if fnmatch.fnmatch(name, pattern):
+                    yield os.path.join(path, name)
+                    break
+        #only deal one level of the dir
+        if single_level:
+            break
+for name in allFiles('weixin-39265957/quiz-w8-data/', single_level = True):
+    print(name)
